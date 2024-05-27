1273f658913ca60d7fb4f766a5084eefc34ade8f
Gabriel Remon <remong@llamas.local>
2024-05-27 13:57:48 -0300

    first commit

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,51 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, model } from '@angular/core';
+import { ModalController, Platform } from '@ionic/angular';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
+import { LensFacing, BarcodeScanner} from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
-  constructor() {}
+  qrActual = "";
+
+  constructor(
+    private plataform: Platform,
+    private modalController: ModalController
+  ) {}
+
+  ngOnInit(): void {
+      if(this.plataform.is("capacitor")){
+
+        BarcodeScanner.isSupported().then();
+        BarcodeScanner.checkPermissions().then();
+        BarcodeScanner.removeAllListeners().then();
+
+      }
+  }
+
+  async scanQr() {
+    const modal = await this.modalController.create({
+    component: BarcodeScanningModalComponent,
+    cssClass: 'barcode-scanning-modal',
+    showBackdrop: false,
+    componentProps: { 
+      formats: [],
+      LensFacing:LensFacing.Back
+     }
+    });
+  
+    await modal.present();
+  
+    const { data } = await modal.onWillDismiss()
+
+    if(data){
+      this.qrActual = data?.barcode?.displayValue
+    }
+
+  }
 
 }
